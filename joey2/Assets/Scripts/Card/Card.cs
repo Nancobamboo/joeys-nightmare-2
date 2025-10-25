419dd7370df8c23c22428832a33a8e77ec32cd66
ze.song <ze.song@garena.com>
2025-10-25 16:56:34 +0800

    a

--- joey2/Assets/Scripts/Card/Card.cs
+++ joey2/Assets/Scripts/Card/Card.cs
@@ -4,58 +4,36 @@ using UnityEngine;
 
 public class Card 
 {
-    public string instanceId;
     public string id;
-    public string name;
-    public string desc;
     public string type;
-    public  Card(string _id, string _name, string _desc, string _type)
+    public string cardImage;
+    public string cardFrame;
+    public string cardName;
+    public string iconType;//image path
+    public string description;
+    public int attack;
+    public int defence;
+    public int health;
+    public int price;
+    public int stars;
+
+    public  Card(string _id, string _type, string _cardImage, string _cardFrame, string _cardName, string _iconType, string _description, int _attack, int _defence, int _health, int _price, int _stars)
     {
         this.id = _id;
-        this.name = _name;
-        this.desc = _desc;
         this.type = _type;
-    }
-
-}
-
-
-public class EnemyCard : Card
-{
-    public int hp;
-    public int current_hp;
-    public int attack;
-    public int current_attack;
-
-    public  EnemyCard(string _id, string _name, string _desc, string _type, int _hp, int _attack) : base(_id, _name, _desc, _type)
-    {
-        this.hp = _hp;
-        this.current_hp = _hp;
+        this.cardImage = _cardImage;
+        this.cardFrame = _cardFrame;
+        this.cardName = _cardName;
+        this.iconType = _iconType;
+        this.description = _description;
         this.attack = _attack;
-        this.current_attack = _attack;
-    }
-
-
-}
-
-
-public class ItemCard : Card
-{
-    public int attack;
-    public int current_attack;
-    public int heal;
-    public int current_heal;
-    public int price;
-    public int current_price;
-    public  ItemCard(string _id, string _name, string _desc,string _type, int _attack, int _heal, int _price) : base(_id, _name, _desc, _type)
-    {
-        this.attack = _attack;
-        this.current_attack = _attack;
-        this.heal = _heal;
-        this.current_heal = _heal;
+        this.defence = _defence;
+        this.health = _health;
         this.price = _price;
-        this.current_price = _price;
+        this.stars = _stars;
     }
+
 }
+
 
 
