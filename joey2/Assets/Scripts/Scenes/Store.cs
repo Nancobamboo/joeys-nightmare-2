3d67829df50f49379c9b49475f6047bc0eaa3e02
ze.song <ze.song@garena.com>
2025-11-11 12:11:14 +0800

    a

--- joey2/Assets/Scripts/Scenes/Store.cs
+++ joey2/Assets/Scripts/Scenes/Store.cs
@@ -11,25 +11,6 @@ public class Store : MonoSingleton<Store>
     public List<Transform> itemList = new List<Transform>();
 
 
-    void Start()
-    {
-        if (PData.Instance.currentLevel > 0)
-        {
-            level = PData.Instance.currentLevel;
-        }
-
-        // 初始化数据（如需要从 GData 抽卡生成怪物/技能/道具等）
-        PhaseManager.Instance.SetGamePhase(GamePhase.battleStart);
-        PData.Instance.SetPlayerHP(PData.Instance.playerHealth);
-    }
-
-
-    public void OnHPChanged(int hp)
-    {
-        StartCoroutine(VFXStackHelper.ChangeJoeyImage(joeyImage: joeyImage));
-    }
-
-
 
 
 }
