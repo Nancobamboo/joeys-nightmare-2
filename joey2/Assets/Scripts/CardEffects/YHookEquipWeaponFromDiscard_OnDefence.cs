ec6606d87b40025465438a24151e744b69e9e01e
tianqiang.yang <ytq1070007954@gmail.com>
2025-11-24 11:18:20 +0800

    Fix Bug

--- joey2/Assets/Scripts/CardEffects/YHookEquipWeaponFromDiscard_OnDefence.cs
+++ joey2/Assets/Scripts/CardEffects/YHookEquipWeaponFromDiscard_OnDefence.cs
@@ -6,24 +6,24 @@ using System;
 using Cysharp.Threading.Tasks;
 using Random = UnityEngine.Random;
 
-public class YHookEquipWeaponFromDiscard_OnDefence : YCardEffect
+public class YHookEquipWeaponFromDiscard_OnDefence : YDefaultEffect
 {
 	public YHookEquipWeaponFromDiscard_OnDefence()
 	{
 		Id = ECardEffectId.HookEquipWeaponFromDiscard_OnDefence;
 	}
 
-	public override float UseDefence(bool isOverflow = false)
-	{
-		if (CardControl != null && CardControl.gameObject != null)
-		{
-			var vfxNames = new List<EVFXName> { EVFXName.VFX_Dun };
-			float maxDelayTime = CardControl.PlayVFX(vfxNames, ECardAnimName.UI_Carditem_dunpai, EVFXLife.SelfLife);
-			SFX.PlayAudio("Audio/SFX/Battle/Defence", 1.0f, 0f);
-			return maxDelayTime > 0f ? maxDelayTime : base.UseDefence(isOverflow);
-		}
-		return base.UseDefence(isOverflow);
-	}
+	//public override float UseDefence(bool isOverflow = false)
+	//{
+	//	if (CardControl != null && CardControl.gameObject != null)
+	//	{
+	//		var vfxNames = new List<EVFXName> { EVFXName.VFX_Dun };
+	//		float maxDelayTime = CardControl.PlayVFX(vfxNames, ECardAnimName.UI_Carditem_dunpai, EVFXLife.SelfLife);
+	//		SFX.PlayAudio("Audio/SFX/Battle/Defence", 1.0f, 0f);
+	//		return maxDelayTime > 0f ? maxDelayTime : base.UseDefence(isOverflow);
+	//	}
+	//	return base.UseDefence(isOverflow);
+	//}
 
 	public override float OnRemoveCard()
 	{
