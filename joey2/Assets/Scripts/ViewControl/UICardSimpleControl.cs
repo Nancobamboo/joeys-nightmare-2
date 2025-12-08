4e860455442219baf76bdf3abf043bd3ff15ea30
tianqiang.yang <ytq1070007954@gmail.com>
2025-12-08 15:22:36 +0800

    fix 大春驴

--- joey2/Assets/Scripts/ViewControl/UICardSimpleControl.cs
+++ joey2/Assets/Scripts/ViewControl/UICardSimpleControl.cs
@@ -2,10 +2,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
-using Cysharp.Threading.Tasks;
-using System.Threading;
 
 public enum ECardType
 {
