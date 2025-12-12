47a417313c147309c54d5d8db1c569010e2bb81f
tianqiang.yang <ytq1070007954@gmail.com>
2025-12-12 20:46:25 +0800

    fix  addeffect 不生效 和手里剑卷轴

--- joey2/Assets/Scripts/CardEffects/YDartScroll.cs
+++ joey2/Assets/Scripts/CardEffects/YDartScroll.cs
@@ -11,14 +11,22 @@ public class YDartScroll : YCardEffect
 
     public override float UseItem()
     {
-        int count = 3;
-        while (count > 0)
-        {
-            Debug.Log("Add dart to env: " + count);
-            YActionSystem.Instance.DispatchAction(EActionId.AddCardToEnv, CardControl, "1003");
-            count--;
-        }
-		return 0f;
+        YActionSystem.Instance.DispatchAction(EActionId.AddCardsToEnvByCardId, CardControl, "1003", 3);
+        return 0f;
     }
 }
 
+public partial class UIGamePhaseControl
+{
+    public void AddCardsToEnvByCardId(UICardSimpleControl cardControl, string cardId, int count)
+    {
+        List<Card> cards = new List<Card>();
+        for (int i = 0; i < count; i++)
+        {
+            Card card = GData.Instance.GetCardConfigById(cardId).Clone();
+            cards.Add(card);
+        }
+        int envIndex = Random.Range(0, m_EnvPanels.Count);
+        AddEnvDropCard(cards, envIndex);
+    }
+}
